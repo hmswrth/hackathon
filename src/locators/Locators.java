3292b843f4cf13d5cbf8575b1aec6a4596318d09
hmswrth <mudra.hemanth@gmail.com>
2020-05-09 19:19:32 +0530

    resolve conflict

---
+++ src/locators/Locators.java
@@ -0,0 +1,62 @@
+package locators;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.JavascriptExecutor;
+import browser.BrowserSetup;
+import jdk.nashorn.internal.scripts.JS;
+
+public class Locators extends BrowserSetup {
+	public static void main(String[] args) throws InterruptedException, IOException {
+		setBrowser();
+		getUrl();
+
+		driver.findElement(By.xpath("//*[@id='header-search-field']")).sendKeys("Web Development", Keys.RETURN);
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+<<<<<<< HEAD
+	
+        WebElement filterElement = driver.findElement(By.xpath("//*[@id=\"udemy\"]/div[2]/div[3]/div/div/div[5]/div[1]/div[1]/div[1]/button"));
+
+		js.executeScript("arguments[0].scrollIntoView(true)",filterElement);
+		
+		filterElement.click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		WebElement beginnerEl = driver.findElement(By.xpath("//*[@id=\"filter-form--13\"]/fieldset[3]/div[2]/label/span"));
+	beginnerEl.click();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	
+	WebElement englishEl = driver.findElement(By.xpath("//*[@id=\"filter-form--13\"]/fieldset[4]/div[1]/label/span"));
+	englishEl.click();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	
+	WebElement doneBtn = driver.findElement(By.xpath("//*[@id=\"udemy\"]/div[2]/div[3]/div/div/div[5]/div[1]/div[1]/div[1]/button[1]"));
+	doneBtn.click();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	
+	for(int i=0; i<3 ; i++) {
+		System.out.println(driver.findElement(By.xpath("//*[@id=\"udemy\"]/div[2]/div[3]/div/div/div[5]/div[2]/div["+(i+1)+"]/div/a/div/div[2]/div[1]/h4")).getText());	
+	}
+	
+		driver.close();
+	
+	}
+}
+=======
+public class Locators extends Browser {
+	public static WebElement searchtTextBox() {    //returns search box webElement
+			WebElement input = driver.findElement(By.xpath("//*[@id='header-search-field']"));
+			return input;	
+     }
+	 public static WebElement selectCourse() { //returns submit webElement
+		 WebElement select =driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[3]/div/div/div/ul/li[1]/a/span"));
+		 return select;
+	 }
+	
+>>>>>>> upstream/master
